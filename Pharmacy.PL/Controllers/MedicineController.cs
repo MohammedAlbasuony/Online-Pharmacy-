90572f75ca642246291d535bb33dc76f576c1e1a
Mohammed Albasuony <mido.beso.hosny@gmail.com>
2025-05-14 02:41:08 +0300

    add dynamic search for medicine

--- Pharmacy.PL/Controllers/MedicineController.cs
+++ Pharmacy.PL/Controllers/MedicineController.cs
@@ -18,10 +18,23 @@ namespace Pharmacy.PL.Controllers
         {
             return View();
         }
-        public async Task<IActionResult> GetAllMedicine(int page = 1)
+        public async Task<IActionResult> GetAllMedicine(string searchQuery, int page = 1)
         {
             int pageSize = 10;  // Define the page size
-            var medicines = await _medicineService.GetAllAsync(); // Assuming GetAllAsync() returns a List
+            var medicines = await _medicineService.GetAllAsync();
+            if (!string.IsNullOrWhiteSpace(searchQuery))
+            {
+                searchQuery = searchQuery.ToLower();
+
+                medicines = medicines.Where(m =>
+                    m.Name.ToLower().Contains(searchQuery) ||
+                    m.Category.ToLower().Contains(searchQuery) ||
+                    m.Manufacturer.ToLower().Contains(searchQuery) ||
+                    m.Uses.ToLower().Contains(searchQuery) ||
+                    m.SideEffects.ToLower().Contains(searchQuery)).ToList();
+            }
+
+            ViewBag.CurrentFilter = searchQuery;
             var pagedMedicines = medicines.ToPagedList(page, pageSize); // Paginate the list
 
             return View(pagedMedicines); // Pass the paginated result to the view
