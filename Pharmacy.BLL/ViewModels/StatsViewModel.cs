baf2eabce2f67428ea90ef825dd79d2b55b2dfe9
Mohammed Albasuony <mido.beso.hosny@gmail.com>
2025-05-14 19:46:36 +0300

    .

---
+++ Pharmacy.BLL/ViewModels/StatsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.BLL.ViewModels
+{
+    public class StatsViewModel
+    {
+        public int DoctorCount { get; set; }
+        public int PharmacistCount { get; set; }
+        public int PatientCount { get; set; }
+    }
+
+}
--- Pharmacy.PL/Controllers/HomeController.cs
+++ Pharmacy.PL/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.BLL.ViewModels;
+using Pharmacy.DAL.DB;
 
 
 namespace Pharmacy.PL.Controllers
@@ -7,21 +10,33 @@ namespace Pharmacy.PL.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context; // Replace 'object' with your actual DbContext type  
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var doctorCount = _context.Doctors.Count();
+            var pharmacistCount = _context.Pharmacists.Count();
+            var patientCount = _context.Patients.Count();
+
+            var stats = new StatsViewModel
+            {
+                DoctorCount = doctorCount,
+                PharmacistCount = pharmacistCount,
+                PatientCount = patientCount
+            };
+
+            return View(stats);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
-
     }
 }
